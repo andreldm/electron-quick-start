e9040e2e8e9e75a0461c14e37acc3cf11e5e3907
Andre Miranda <andre@andreldm.com>
2021-08-17 18:03:09 -0300

    Add single instance behavior

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+let mainWindow = null
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -19,6 +20,10 @@ function createWindow () {
   // mainWindow.webContents.openDevTools()
 }
 
+if (!app.requestSingleInstanceLock()) {
+  app.quit()
+}
+else {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -39,5 +44,11 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('second-instance', (event, commandLine, workingDirectory) => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore()
+    mainWindow.focus()
+  }
+})
+}
